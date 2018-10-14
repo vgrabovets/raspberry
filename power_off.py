6399bd173a6f021c648e30243495694c25a8edc9
Vitaliy Grabovets <v.grabovets@gmail.com>
2018-10-14 13:48:43 +0300

    add led

--- power_off.py
+++ power_off.py
@@ -5,8 +5,12 @@ import RPi.GPIO as GPIO
 
 
 GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
+GPIO.setup(17, GPIO.OUT)
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.output(17, True)
 
 while True:
     input_state = GPIO.input(18)
